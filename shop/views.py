d6e8bc6440dcdf6f96aa103e8f49a2d49eeabdca
00014039wiut <00014039wiut.uz>
2025-01-11 21:33:22 +0500

    Minor changes

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from rest_framework.generics import ListAPIView
 
 from shop.models import Category, Product, CustomUser
 from shop.serializers import CategorySerializer, ProductSerializer, CustomUserSerializer
+from rest_framework.pagination import PageNumberPagination
+
+
+class ProductPagination(PageNumberPagination):
+    page_size = 10
 
 
 # Create your views here.
@@ -14,8 +19,11 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
 
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
+    queryset = Product.objects.select_related('category').all()
     serializer_class = ProductSerializer
+    pagination_class = ProductPagination
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['price', 'name']
 
 
 class UserViewSet(viewsets.ModelViewSet):
